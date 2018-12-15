325e47bdcdd65c1962868f9d72a803b06db42bf3
mwieckowsk <32486835+mwieckowsk@users.noreply.github.com>
2018-12-15 16:32:47 +0100

    Raw AgentMatrix

---
+++ PopulationsProtocols/AgentMatrix.cs
@@ -0,0 +1,12 @@
+﻿namespace PopulationsProtocols
+{
+    internal class AgentMatrix
+    {
+        private int x;
+
+        public AgentMatrix(int x)
+        {
+            this.x = x;
+        }
+    }
+}
