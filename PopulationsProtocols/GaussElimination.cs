3ceeabdc1d6027585659e57e7b323dcef40c1bf6
mwieckowsk <32486835+mwieckowsk@users.noreply.github.com>
2018-12-17 16:35:49 +0100

    Update GaussElimination.cs

--- PopulationsProtocols/GaussElimination.cs
+++ PopulationsProtocols/GaussElimination.cs
@@ -9,17 +9,16 @@ namespace PopulationsProtocols
     {
         protected MyMatrix matrix;
         protected MyMatrix vector;
-        protected int matrixSize;
         protected abstract void CreateUpperTriangularMatrix();
 
         protected GaussElimination(MyMatrix matrix, MyMatrix vector)
         {
             this.matrix = matrix;
             this.vector = vector;
-        }
+    }
         protected void NullifyDownOfColumn(int column, bool sparse)
         {
-            for (int row = column + 1; row < matrixSize; row++)
+            for (int row = column + 1; row < (column * row); row++)
             {
                 if (sparse && matrix.IsZero(row, column)) continue;
                 double factor = matrix[row][column] / matrix[column][column];
@@ -33,11 +32,29 @@ namespace PopulationsProtocols
         }
         private void UpdateRow(int column, int row, double factor)
         {
-            for (int rowElement = column; rowElement < matrixSize; rowElement++)
+            for (int rowElement = column; rowElement < (column * row); rowElement++)
             {
                 double newMatrixValue = matrix[row][rowElement] - (matrix[column][rowElement] * factor);
                 matrix[row][rowElement] = newMatrixValue;
 
+            }
+        }
+        
+        protected void backwardSubstitution(int row)
+        {
+            for (int columnElement = (row * row) - 1; columnElement >= 0; columnElement--)
+            {
+                double matrixValue = matrix[columnElement][columnElement];
+                double basicResult = vector[columnElement, 0];
+                vector[columnElement, 0] = vector[columnElement, 0] / matrixValue;
+                matrix[columnElement][columnElement] = 1.0;
+                for (int rowElement = columnElement - 1; rowElement >= 0; rowElement--)
+                {
+                    double fraction = matrix[rowElement, columnElement]/matrixValue;
+                    double result = vector[rowElement, 0] - basicResult*fraction;
+                    vector[rowElement, 0] = result;
+                    matrix[rowElement][columnElement] = 0.0;
+                }
             }
         }
     }
